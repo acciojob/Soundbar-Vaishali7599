9073a2d7564c2ac0b10d8547e19b6af0bfff5c29
Vaishali7599 <140495282+Vaishali7599@users.noreply.github.com>
2024-12-14 22:56:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let btns = document.querySelectorAll(".btn");
+let currentAudio = null;
+
+btns.forEach(t => {
+    let soundId = t.getAttribute("data-sound");
+    let audio = document.getElementById(soundId);
+    
+    t.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (currentAudio) {
+            currentAudio.pause();
+            currentAudio.currentTime = 0;
+        }
+        currentAudio = audio;
+        currentAudio.play();
+    });
+});
+
+let stop = document.querySelector(".stop");
+
+stop.addEventListener("click", () => {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+});
